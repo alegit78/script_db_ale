2d589a9b13b57dd3ce679e68f36eda4c217ba5d6
Verrua Alessandra <008468@ict.corp.rai.it>
2019-04-15 15:50:50 +0200

    Signed-off-by: Verrua Alessandra <008468@ict.corp.rai.it>

--- create_table.sh
+++ create_table.sh
@@ -1,6 +1,6 @@
 ORACLE_SID=siprages;
 export ORACLE_SID;
-/opt/app/oracle/product/12.1.0/client_1/bin/sqlplus jarc/jarc@LTOCAV446_JENKINS.world
+/opt/app/oracle/product/12.1.0/client_1/bin/sqlplus jarc/jarc@LTOCAV446_JENKINS.world @prova.sql
 
-create table prova_ale (nn number);
+
 
